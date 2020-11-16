e9114b7e536e722584652fbd3b85da1badd11d54
raffaella <suardini.raffaella@gmail.com>
2020-11-16 19:46:21 +0100

    update settings for static_url

--- progetto/settings.py
+++ progetto/settings.py
@@ -17,6 +17,7 @@ from pathlib import Path, os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
